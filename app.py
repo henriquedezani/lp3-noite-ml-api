c8e931f1ab592217c754c09e72af81ec0a17e814
Henrique Dezani <dezani@gmail.com>
2021-06-07 23:39:04 +0000

    Aula do dia 07/06/21

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
+
+# @app.route('/<name>')
+# def home(name):
+#     return render_template('hello.html', name=name)
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home():
+    return render_template('hello.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['search']
+    # TODO: Usar o modelo de ML já treinado para predizer a resposta.
+    # modelo.predict(query)
+    return jsonify({
+        "query": query,
+        "result": "positivo"
+    })
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
